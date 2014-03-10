d3ccb7a37fbb91c90189b8a94631f7018cf455c6
odf <olaf.delgado@gmail.com>
2014-03-10 17:47:42 +1100

    define saner behaviour for Ringbuffer read and resize

--- index.js
+++ index.js
@@ -35,16 +35,24 @@ RingBuffer.prototype.write = function(val) {
 };
 
 RingBuffer.prototype.read = function() {
-  var val = this.data[this.data_start];
-  this.data_start = (this.data_start + 1) % this.size;
-  this.data_count = Math.max(this.data_count - 1, 0);
-  return val;
+  if (this.data_count > 0) {
+    var val = this.data[this.data_start];
+    this.data_start = (this.data_start + 1) % this.size;
+    this.data_count = Math.max(this.data_count - 1, 0);
+    return val;
+  }
 };
 
 RingBuffer.prototype.resize = function(n) {
   var new_data = new Array(n);
-  for (var i = 0; i < this.data_count; ++i)
-    new_data[i % n] = this.data[(this.data_start + i) % this.size];
+  if (n < this.data_count) {
+    var base = this.data_start + this.data_count - n;
+    for (var i = 0; i < n; ++i)
+      new_data[i] = this.data[(base + i) % this.size];
+  }
+  else
+    for (var i = 0; i < this.data_count; ++i)
+      new_data[i] = this.data[(this.data_start + i) % this.size];
   this.size = n;
   this.data_start = 0;
   this.data_count = Math.min(this.data_count, this.size);
